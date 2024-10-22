95622d339c2e317d17c9614efe007e9506d270cf
evelyncrystine <163007707+evelyncrystine@users.noreply.github.com>
2024-10-22 08:47:26 -0300

    .

--- main.js
+++ main.js
@@ -48,3 +48,20 @@ let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
         document.body.appendChild(botaoReiniciar);
         botaoReiniciar.addEventListener('click', reiniciarJogo);
     }
+
+
+function reiniciarJogo() {
+    contagemPalpites = 1;
+    const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultado p');
+    for (const paragrafoReiniciar of paragrafosReiniciar) {
+        paragrafoReiniciar.textContent = "";
+    }
+
+    botaoReiniciar.parentNode.removeChild(botaoReiniciar);
+    campoPalpite.disabled = false;
+    envioPalpite.disabled = false;
+    campoPalpite.value = "";
+    campoPalpite.focus();
+    ultimoResultado.style.backgroundColor = 'white';
+    numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+}
